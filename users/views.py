0f0f3cf2c0e92b4c1d8ce245a24c2cb7cc737728
ivanprytula <ivanprytula87@gmail.com>
2020-06-14 15:34:24 +0300

    Add simple protected view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -8,3 +10,9 @@ class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+@login_required
+def protected_view(request):
+    """A view that can only be accessed by logged-in users"""
+    return render(request, 'protected.html', {'current_user': request.user})
